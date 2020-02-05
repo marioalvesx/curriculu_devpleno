2ce86b13ef1ce4a90f95b09e0ec5a9b7993b3464
Mario Alves <marioalvesneto@hotmail.com>
2020-02-05 03:05:03 -0300

    Creating the resume

--- app.js
+++ app.js
@@ -3,8 +3,15 @@ const port = process.env.port || 3000
 
 const app = express();
 
+app.set('view engine', 'ejs')
+
 app.get('/', (req, res) => {
     res.render('home')
+})
+
+// Redireciona para o arquivo do curriculo na pasta resume
+app.get('/resume', (req, res) => {
+    res.render('resume/index')
 })
 
 app.listen(port, error => {
